4d554fe155ee7af1b1039ec64ba5c66b7ab847a7
Scott Wegner <swegner@google.com>
2016-04-01 14:11:22 -0700

    Move hasNamespace to DisplayDataMatchers

--- sdks/java/core/src/test/java/com/google/cloud/dataflow/sdk/transforms/display/DisplayDataMatchersTest.java
+++ sdks/java/core/src/test/java/com/google/cloud/dataflow/sdk/transforms/display/DisplayDataMatchersTest.java
@@ -19,6 +19,7 @@ package com.google.cloud.dataflow.sdk.transforms.display;
 
 import static com.google.cloud.dataflow.sdk.transforms.display.DisplayDataMatchers.hasDisplayItem;
 import static com.google.cloud.dataflow.sdk.transforms.display.DisplayDataMatchers.hasKey;
+import static com.google.cloud.dataflow.sdk.transforms.display.DisplayDataMatchers.hasNamespace;
 import static com.google.cloud.dataflow.sdk.transforms.display.DisplayDataMatchers.hasType;
 import static com.google.cloud.dataflow.sdk.transforms.display.DisplayDataMatchers.hasValue;
 import static com.google.cloud.dataflow.sdk.transforms.display.DisplayDataMatchers.includes;
@@ -27,6 +28,7 @@ import static org.hamcrest.core.StringStartsWith.startsWith;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertThat;
 
+import com.google.cloud.dataflow.sdk.transforms.DoFn;
 import com.google.cloud.dataflow.sdk.transforms.PTransform;
 import com.google.cloud.dataflow.sdk.transforms.display.DisplayData.Builder;
 import com.google.cloud.dataflow.sdk.values.PCollection;
@@ -97,6 +99,15 @@ public class DisplayDataMatchersTest {
   }
 
   @Test
+  public void testHasNamespace() {
+    Matcher<DisplayData> matcher = hasDisplayItem(hasNamespace(SampleTransform.class));
+
+    assertFalse(matcher.matches(DisplayData.from(
+        new PTransform<PCollection<String>, PCollection<String>>(){})));
+    assertThat(createDisplayDataWithItem("foo", "bar"), matcher);
+  }
+
+  @Test
   public void testIncludes() {
     final HasDisplayData subComponent = new HasDisplayData() {
       @Override
@@ -125,13 +136,23 @@ public class DisplayDataMatchersTest {
     assertThat(DisplayData.from(subComponent), matcher);
   }
 
+
   private DisplayData createDisplayDataWithItem(final String key, final String value) {
-    return DisplayData.from(
-        new PTransform<PCollection<String>, PCollection<String>>() {
-          @Override
-          public void populateDisplayData(Builder builder) {
-            builder.add(key, value);
-          }
-        });
+    return DisplayData.from(new SampleTransform(key, value));
+  }
+
+  static class SampleTransform extends PTransform<PCollection<String>, PCollection<String>> {
+    private final String key;
+    private final String value;
+
+    SampleTransform(String key, String value) {
+      this.key = key;
+      this.value = value;
+    }
+
+    @Override
+    public void populateDisplayData(Builder builder) {
+      builder.add(key, value);
+    }
   }
 }
